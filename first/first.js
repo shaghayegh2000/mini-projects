14212786d2b21fce68c638f93253df8edd0a7039
shaghaygh <shqayqm145@gmail.com>
2023-04-09 18:31:35 +0430

    9/4/2023 , 18:30

--- first/first.js
+++ first/first.js
@@ -6,13 +6,14 @@ var holdTime = totTime/5 ;
 
 var breathAnimation = ()=> {
     txt.innerHTML = "Breathe In!"
+    container.className = "container grow"
     setTimeout(() => {
         txt.innerHTML = "Hold!"
         setTimeout(()=>{
             txt.innerHTML = "Breathe Out!"
+            container.className = "container shrink"
         },holdTime)
     },breathTime)
 }
 breathAnimation();
-
 setInterval(breathAnimation , totTime);
