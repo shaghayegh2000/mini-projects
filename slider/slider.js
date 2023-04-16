2c67d700cf00e72d403bac3a1ed3f88c49e4c1fb
shaghaygh <shqayqm145@gmail.com>
2023-04-16 12:11:21 +0430

    16/4/2023 , 12:11

--- slider/slider.js
+++ slider/slider.js
@@ -1,7 +1,2 @@
-// let sliderImg = document.getElementById('img');
-// sliderImg.style.background = "url('../imgs/perori-00.jpg')";
-// sliderImg.style.backgroundSize = "contain";
-// sliderImg.style.backgroundRepeat = "no-repeat";
-
 
 
