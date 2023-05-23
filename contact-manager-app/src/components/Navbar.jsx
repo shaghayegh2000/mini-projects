a7e45b7bdaeaf8167a39076895da5a1f773ab82b
shaghaygh <shqayqm145@gmail.com>
2023-05-23 22:31:16 +0330

    23/5/2023 , 10:15

--- contact-manager-app/src/components/Navbar.jsx
+++ contact-manager-app/src/components/Navbar.jsx
@@ -3,8 +3,23 @@ const Navbar = () => {
         <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
             <div className="container">
                 <div className="row w-100">
+                    <div className="col mt-2">
+                        <i className="fa fa-id-badge"></i>
+                        {" "}Contact manager web application 
+                        <span className="contacts">Contacts</span>
+                    </div>
                     <div className="col">
-                        <i className="fas fa-id-badge"></i>
+                        <div className="input-group mx-2 w-75">
+                            <span className="px-3 input-group-text input" id="basic-addon1">
+                                <i className="fa fa-search"></i>
+                            </span>
+                            <input 
+                            className="rounded-2 form-control textInput" 
+                            type="text" 
+                            placeholder="Search contact"
+                            aria-label="search"
+                            aria-describedby="basic-addon1"/>
+                        </div>
                     </div>
                 </div>
             </div>
