20681ee450b98cc86d54bf4f1b5db1ae269329be
shaghaygh <shqayqm145@gmail.com>
2023-05-23 20:45:48 +0330

    23/5/2023 , 20:33

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className="App">
-      <h3>Mange Contact Application</h3>
-      <span className='fa fa-user-secret'></span>
-      <button className='btn btn-primary'>click</button>
+      <Navbar/>
     </div>
   );
 }
---
+++ contact-manager-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return(
+        <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
+            <div className="container">
+                <div className="row w-100">
+                    <div className="col">
+                        <i className="fas fa-id-badge"></i>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
