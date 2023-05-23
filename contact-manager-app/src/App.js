3ed6a6e87d3989b085a71dd4b499d089b2c08751
shaghaygh <shqayqm145@gmail.com>
2023-05-23 19:29:56 +0330

    23/5/2023 , 19:12

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div className="App">
       <h3>Mange Contact Application</h3>
+      <span className='fa fa-user-secret'></span>
+      <button className='btn btn-primary'>click</button>
     </div>
   );
 }
--- contact-manager-app/src/index.js
+++ contact-manager-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// //bootstrap
+// import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '/node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
