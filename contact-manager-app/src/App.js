771d114d28aed47ce1861d410e3dbe32bc20d21f
shaghaygh <shqayqm145@gmail.com>
2023-06-02 10:55:28 +0330

    2/6/2023 , 10:33

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Contacts from './components/contact/Contacts';
 
 const App = () => {
+
+  const [contacts , setContact] = useState([]);
   return (
     <div className="App">
       <Navbar/>
