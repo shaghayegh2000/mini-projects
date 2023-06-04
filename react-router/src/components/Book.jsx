782ab435dca3c37e3a441ade2897696189048516
shaghaygh <shqayqm145@gmail.com>
2023-06-04 15:12:04 +0330

    4/6/2023 , 15:02

--- react-router/src/components/Book.jsx
+++ react-router/src/components/Book.jsx
@@ -4,20 +4,29 @@ import {getBook} from "../data/data";
 const Book = () => {
     const params = useParams();
     const book = getBook(parseInt(params.bookId));
-    return(
-        <main className=" rounded-pill bg-info bg-opacity-25 w-50 mx-auto mt-2 mb-2">
-            <h3 className="pt-4">Total Price : {`${book.amount} Tooman`}</h3>
-            <p className="mt-2">
-                {book.name}
-            </p>
-            <p className="mt-2">
-                Date : {book.due}
-            </p>
-            <p className="btn">
-                <button className="btn btn-dark btn-outline-info">Delete the book</button>
-            </p>
+    if(book){
+        return(
+            <main className=" rounded-pill bg-info bg-opacity-25 w-50 mx-auto mt-2 mb-2">
+                <h3 className="pt-4">Total Price : {`${book.amount} Tooman`}</h3>
+                <p className="mt-2">
+                    {book.name}
+                </p>
+                <p className="mt-2">
+                    Date : {book.due}
+                </p>
+                <p className="btn">
+                    <button className="btn btn-dark btn-outline-info">Delete the book</button>
+                </p>
+            </main>
+        )
+    }else {
+        return (
+        <main className=" rounded-pill bg-info bg-opacity-25 w-50 py-5 mx-auto my-auto">
+            <h3>The book not found!</h3>
         </main>
-    )
+        )
+    }
+
 }
 
 export default Book;
--- react-router/src/index.js
+++ react-router/src/index.js
@@ -19,9 +19,21 @@ root.render(
       <Routes>
         <Route path="/" element={<App/>}>
           <Route path="/books" element={<Books/>}>
+            <Route index element={
+              <main>
+                <p>Choose your book</p>
+              </main>
+            }></Route>
           <Route path=':bookId' element={<Book/>}></Route>
           </Route>
           <Route path="/about" element={<About/>}></Route>
+          <Route path='*' element={
+          <div className='d-flex justify-content-center'>
+            <main className='d-flex position-absolute top-50'>
+              <h2>Page Not Found</h2>
+            </main>
+          </div>
+        }></Route>
         </Route>
         {/* <Route path="/books" element={<Books/>}></Route>
         <Route path="/about" element={<About/>}></Route> */}
