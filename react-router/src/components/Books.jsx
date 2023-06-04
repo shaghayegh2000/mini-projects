0ec0d160d58d80fb88bf11e5e2e76dd45ab596fd
shaghaygh <shqayqm145@gmail.com>
2023-06-04 16:32:41 +0330

    4/6/2023 , 16:22

--- react-router/src/components/Books.jsx
+++ react-router/src/components/Books.jsx
@@ -1,21 +1,41 @@
 import {getBooks} from "../data/data";
-import { Link , Outlet } from "react-router-dom";
+import { NavLink , Outlet , useSearchParams } from "react-router-dom";
 
 const Books = () => {
+
+  const [searchPara , setSearchPara] = useSearchParams();
+
     const books = getBooks();
   
     return (
       <div className="d-flex rounded py-4 w-75 mt-3 mx-auto bg-info bg-opacity-25">
         <nav className="d-flex flex-column">
-          <input className="mb-4 mx-5" type="text" placeholder="Search Book" />
-          {books.map((book) => (
-            <Link
+          <input onChange={event => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchPara({filter : filter})
+            }else {
+              setSearchPara({});
+            }
+          }} value={searchPara.get('filter') || ""} className="mb-4 mx-5" type="text" placeholder="Search Book" />
+          {books.filter((book)=>{
+            let filter = searchPara.get('filter');
+            if(!filter) return true;
+            let name = book.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          books.map((book) => (
+            <NavLink style={({isActive}) => {
+              return {
+                color: isActive ? "red" : "",
+              }
+            }}
               to={`/books/${book.number}`}
               key={book.number}
               className=" mt-2 mx-2"
             >
               {book.name}
-            </Link>
+            </NavLink>
           ))}
         </nav>
         <Outlet/>
