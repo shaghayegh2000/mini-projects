ccdcd41484128d29f55b552a7cd75e41a0869de6
shaghaygh <shqayqm145@gmail.com>
2023-06-05 19:06:52 +0330

    5/6/2023 , 19:01

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,3 +1,20 @@
-async function getQuotes() {
-    
+let apiQuotes = [];
+
+let newQuote =() => {
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    alert(quote);
 }
+newQuote();
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json(); 
+        newQuote();
+    } catch (error) {
+        alert('error');
+    }
+}
+
+getQuotes();
