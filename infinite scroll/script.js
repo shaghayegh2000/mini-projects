042f84d1eb800bc0881cdb609455771aa5709483
shaghaygh <shqayqm145@gmail.com>
2023-07-09 18:40:30 +0330

    9/7/2023 , 18:03

--- infinite scroll/script.js
+++ infinite scroll/script.js
@@ -1,17 +1,36 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let photoArray = [];
+const img = document.createElement('img');
+
+
 const count = 10;
 const apiKey = 'API_KEY';
-const apiUrl = `http://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function displayPhoto() {
+    photoArray.forEach((photo)=>{
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.link.html);
+        item.setAttribute('target' , '_blank');
+        img.setAttribute('src' , photo.urls.regular);
+        img.setAttribute('alt' , photo.alt_description);
+        img.setAttribute('title' , photo.alt_description);
+        
+    })
+}
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photoArray = await response.json();
+        displayPhoto();
     } catch (error) {
         
     }
 }
 getPhotos();
+
 
 
 
