37f6037bf0c752ceb7f802d0f359a4aa622fb8e9
shaghaygh <shqayqm145@gmail.com>
2023-07-12 13:23:08 +0330

    12/7/2023 , 13:15

--- dark mode/script.js
+++ dark mode/script.js
@@ -28,11 +28,22 @@ function lightMode() {
 function switchTheme(ev) {
     if (ev.target.checked) {
         document.documentElement.setAttribute('data-theme' , 'dark');
+        localStorage.setItem('theme' , 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme' , 'light');
+        localStorage.setItem('theme' , 'dark');
         lightMode();
     }
 }
 
 toggleSwitch.addEventListener('change' , switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme' , currentTheme );
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
